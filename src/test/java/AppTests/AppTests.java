66e751326300b09a5053261bc6b7c7bb1c6d7bbb
EvkovicUniversity <61968181+EvkovicUniversity@users.noreply.github.com>
2020-09-26 15:31:09 +0200

    moet weer even committen

--- src/test/java/AppTests/AppTests.java
+++ src/test/java/AppTests/AppTests.java
@@ -9,29 +9,26 @@ public class AppTests {
 
     @Test
     void maakEenReservering(){
-        Scanner naam = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welkom bij 'T Restaurantje. Hier kunt u een reservering maken.");
         System.out.println("Voer een voor- en achternaam in: ");
-        String naamGast = naam.nextLine();
+        String naamGast = scanner.nextLine();
 
-        Scanner leeftijd = new Scanner(System.in);
         System.out.println("Wat is uw leeftijd?");
-        String leeftijdGast = leeftijd.nextLine();
+        String leeftijdGast = scanner.nextLine();
 
-        Scanner aantal = new Scanner(System.in);
         System.out.println("Voor hoeveel mensen wilt u reserveren?");
-        String aantalGasten = aantal.nextLine();
+        String aantalGasten = scanner.nextLine();
 
-
-        Scanner email = new Scanner(System.in);
         System.out.println("Tot slot, wat is uw emailadres?");
-        String emailadres = email.nextLine();
+        String emailadres = scanner.nextLine();
 
         System.out.println("overzicht van gegevens:");
         System.out.println(naamGast);
         System.out.println(leeftijdGast);
         System.out.println(aantalGasten);
         System.out.println(emailadres);
+
 
 
     }
