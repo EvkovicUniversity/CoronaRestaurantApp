ffb69762773eca34d49803d06a30def28912beda
EvkovicUniversity <61968181+EvkovicUniversity@users.noreply.github.com>
2020-09-26 14:49:00 +0200

    ...message...

--- src/main/java/CoronaRestaurantApp/Persoon.java
+++ src/main/java/CoronaRestaurantApp/Persoon.java
@@ -2,13 +2,4 @@ package CoronaRestaurantApp;
 
 public class Persoon {
 
-    public int id;
-    public String naam;
-    byte leeftijd;
-    String email;
-
-    void nieuwPersoon(){
-
-    }
-
 }
